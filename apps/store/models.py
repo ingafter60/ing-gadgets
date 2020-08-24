675e61bc4d75a0fb884fb9ed746fa55fa0231439
ing <inyoman_gurnitha@yahoo.com>
2020-08-24 08:03:08 +0700

    14. Display product by last added

--- apps/store/models.py
+++ apps/store/models.py
@@ -20,6 +20,10 @@ class Product(models.Model):
     slug = models.SlugField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date_added',)
 
     def __str__(self):
         return self.title
